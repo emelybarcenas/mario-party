661c99b4768a50f1d68862756279fcc9ed419dfe
emelybarcenas <emelyxbarcenas@gmail.com>
2025-04-21 19:18:18 -0400

    images

--- mario-party-app/src/components/CharacterCard.jsx
+++ mario-party-app/src/components/CharacterCard.jsx
@@ -1,19 +1,17 @@
 import Form from "./Form"
 export default function CharacterCard({name, characterType}){
-    const characterImages = [
-        {
-            Mario: "/src/assets/mario.png",
-            Luigi: "/src/assets/luigi.png",
-            Peach: "/src/assets/peach.png",
-            Bowser: "/src/assets/bowser.png",
-            Yoshi: "/src/assets/yoshi.png",
-            Toad: "/src/assets/toad.png", 
-        }
-    ]
+    const characterImages = {
+            Mario: "/mario.png",
+            Luigi: "/public/luigi.png",
+            Peach: "/peach.png",
+            Bowser: "/bowser.png",
+            Yoshi: "/yoshi.png",
+            Toad: "/toad.png", 
+    }
     const imageSrc = characterImages[name];
     return(
         <div className="w-[300px] h-[200px] border-2 border-gray-400">
-            <img src={imageSrc} />
+            <img src={imageSrc} alt={name}/>
             <h1>Name: {name}</h1>
             <h1>Character Type: {characterType}</h1>
             
--- mario-party-app/src/components/Home.jsx
+++ mario-party-app/src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home(){
 <div className="scroll-m-0 pt-30">
 <h1 className='font-bold text-4xl text-red-600 '>Let's Party!</h1>
 <p className='p-10 flex flex-col'>Build your dream team from the Mushroom Kingdom! <span>Choose your ultimate Mario Squad from your favorite characters.</span></p>
-<img src="/src/assets/mario-squad.svg" alt="Mario squad" className="w-full h-[500px]"/>
+<img src="/mario-squad.svg" alt="Mario squad" className="w-full h-[500px]"/>
 </div>
     )
 }
